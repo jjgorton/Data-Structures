eef7610418e07603d1ee0ed01ab1ed0e7afec3ef
jjgorton <jjgorton@gmail.com>
2019-12-19 22:30:52 -0500

    commented in size adjustment for Queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -11,12 +11,12 @@ class Queue:
 
     def enqueue(self, value):
         self.storage.add_to_head(value)
-        # self.size += 1
+        self.size += 1
 
     def dequeue(self):
         if self.storage.length > 0:
             return self.storage.remove_from_tail()
-            # self.size -= 1
+            self.size -= 1
         # else:
         #     return 
 
